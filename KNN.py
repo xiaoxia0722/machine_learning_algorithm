34c689d9e7394bc346857970ec12d73912c90993
xiaoxia0722 <2861024198@qq.com>
2020-07-13 18:33:27 +0800

    更新knn算法

--- KNN.py
+++ KNN.py
@@ -6,29 +6,26 @@
 # @File     : KNN.py
 import numpy as np
 from collections import Counter
+from math import sqrt
 
 
-def KNN(unknown_point: list, data_set: np.ndarray, labels: list, k: int) -> str:
+def KNN(unknown_point: np.ndarray, data_set: np.ndarray, labels: np.ndarray, k: int) -> str:
     """
-        将点unknown_point使用KNN(K-近邻算法),根据data_set数据进行分类,以最近的k个点为准
+    将点unknown_point使用KNN(K-近邻算法),根据data_set数据进行分类,以最近的k个点为准
     """
-    # 获取已知分类结果的点的数量
-    data_set_size = len(data_set)
-    # 将unknown_point扩展为data_set_size维,并求得与每个点的差
-    unknown_point = np.tile(unknown_point, (data_set_size, 1)) - data_set
-    # 将差进行平方，得到x方和y方
-    sq_point = unknown_point ** 2
-    # 将每个维度进行求和，即求得x和y的平方和
-    sq_distances = sq_point.sum(axis=1)
-    # 对平方和进行开方，求得距离
-    distances = sq_distances ** 0.5
-    # 对距离进行排序，得到排序后相应的下标
-    sort_distance = distances.argsort()
-    # 找到前k个点
-    k_number = [sort_distance[i] for i in sort_distance[:k]]
-    # 统计前k个点的类别数量
-    votes = Counter(k_number)
-    # 返回最大的数量的标签
-    return votes.most_common(1)[0][0]
+    # 对传入的参数进行判断
+    assert 1 <= k <= data_set.shape[0], "k必须是有效的"
+    assert data_set.shape[0] == labels.shape[0], '训练数据和标签数量必须相同'
+    assert unknown_point.shape[0] == data_set.shape[1], '待分类数据需要和训练数据特征数相同'
 
+    # 计算未知点与训练数据集的距离
+    distance = [sqrt(np.sum((data - unknown_point)**2)) for data in data_set]
+    # 对距离进行排序
+    distance_argsort = np.argsort(distance)
 
+    # 获取距离最近的前k个数据的标签
+    topk_label = [labels[i] for i in distance_argsort[:k]]
+    # 统计每种标签的数量
+    labels_count = Counter(topk_label)
+    # 返回数量最多的标签(未知点的分类结果)
+    return labels_count.most_common(1)[0][0]
