55d7f4a6c0b02d2c58ac5fde3f054fc786b8fc0b
xiaoxia0722 <2861024198@qq.com>
2020-07-13 18:17:28 +0800

    更新knn算法

--- KNN.py
+++ KNN.py
@@ -5,6 +5,7 @@
 # @Blog     : https://xiaoxiablogs.top
 # @File     : KNN.py
 import numpy as np
+from collections import Counter
 
 
 def KNN(unknown_point: list, data_set: np.ndarray, labels: list, k: int) -> str:
@@ -23,20 +24,11 @@ def KNN(unknown_point: list, data_set: np.ndarray, labels: list, k: int) -> str:
     distances = sq_distances ** 0.5
     # 对距离进行排序，得到排序后相应的下标
     sort_distance = distances.argsort()
-    # 定义一个字典用于存储前k个点的类别数量
-    labels_number = {}
-
-    # 遍历前n个点并且记录他们的类别
-    for i in range(k):
-        i_label = labels[sort_distance[i]]
-        # 将该点对应的类别在字典中增加1，不存在则设置为0并且增加1
-        # dict.get()方法用于获取值，第一个参数为要获取的键，第二个参数为字典中没有则要设置成的值
-        labels_number[i_label] = labels_number.get(i_label, 0) + 1
-    # 对字典中的键值进行降序排序，并且返回(键,值)对列表
-    sort_labels_list = sorted(labels_number.items(), key=lambda a: a[1], reverse=True)
-    # 将排名最多的类别返回
-    return sort_labels_list[0][0]
-
-
+    # 找到前k个点
+    k_number = [sort_distance[i] for i in sort_distance[:k]]
+    # 统计前k个点的类别数量
+    votes = Counter(k_number)
+    # 返回最大的数量的标签
+    return votes.most_common(1)[0][0]
 
 
