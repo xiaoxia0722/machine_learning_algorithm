62ed47194e2cb53cb6273d456f526569d9de59c2
xiaoxia0722 <2861024198@qq.com>
2020-07-13 19:51:06 +0800

    添加knn算法类

--- KNN.py
+++ KNN.py
@@ -29,3 +29,41 @@ def KNN(unknown_point: np.ndarray, data_set: np.ndarray, labels: np.ndarray, k: 
     labels_count = Counter(topk_label)
     # 返回数量最多的标签(未知点的分类结果)
     return labels_count.most_common(1)[0][0]
+
+
+class KNNClassifier:
+    def __init__(self, k: int):
+        assert 1 <= k, "k必须是有效的"
+        self.k = k
+        self._X_train = np.array([])
+        self._y_train = np.array([])
+
+    def fit(self, X_train: np.ndarray, y_train: np.ndarray):
+        assert X_train.shape[0] == y_train.shape[0], '训练数据和标签数量必须相同'
+        assert self.k <= X_train.shape[0], "训练数据集的数量必须大于k"
+        self._X_train = X_train
+        self._y_train = y_train
+        return self
+
+    def predict(self, X_predict: np.ndarray):
+        assert self._X_train is not None and self._y_train is not None, "必须训练这个模型"
+        assert X_predict.shape[1] == self._X_train.shape[1], "预测数据必须与训练数据的特征数相同"
+
+        y_predict = []
+
+        for x in X_predict:
+            # 计算未知点与训练数据集的距离
+            distance = [sqrt(np.sum((data - x) ** 2)) for data in self._X_train]
+            # 对距离进行排序
+            distance_argsort = np.argsort(distance)
+
+            # 获取距离最近的前k个数据的标签
+            topk_label = [self._y_train[i] for i in distance_argsort[:self.k]]
+            # 统计每种标签的数量
+            labels_count = Counter(topk_label)
+            # 返回数量最多的标签(未知点的分类结果)
+            y_predict.append(labels_count.most_common(1)[0][0])
+        return np.array(y_predict)
+
+    def __repr__(self):
+        return "KNN(k=%d" % self.k
